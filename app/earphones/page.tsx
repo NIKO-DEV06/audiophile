d66a1cab7482fa5a72b88edaa0209e76d5d57201
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-06-19 21:45:06 +0100

    added headphones, earphones and speakers pages

--- app/earphones/page.tsx
+++ app/earphones/page.tsx
@@ -1,9 +1,19 @@
 import ProductPage from "@/components/products/ProductPage";
+import yx1 from "@/assets/product-yx1-earphones/tablet/image-category-page-preview.jpg";
 
 const EarphonesPage = () => {
   const heading = "earphones";
 
-  return <ProductPage heading={heading} />;
+  const products = [
+    {
+      img: yx1,
+      name: "YX1 WIRELESS EARPHONES",
+      desc: "Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature.",
+      link: "/earphones/yx1-earphone",
+    },
+  ];
+
+  return <ProductPage heading={heading} products={products} />;
 };
 
 export default EarphonesPage;
--- app/speakers/page.tsx
+++ app/speakers/page.tsx
@@ -1,9 +1,25 @@
 import ProductPage from "@/components/products/ProductPage";
+import zx9 from "@/assets/product-zx9-speaker/tablet/image-category-page-preview.jpg";
+import zx7 from "@/assets/product-zx7-speaker/tablet/image-category-page-preview.jpg";
 
 const SpeakersPage = () => {
   const heading = "speakers";
+  const products = [
+    {
+      img: zx9,
+      name: "ZX9 speaker",
+      desc: "Upgrade your sound system with the all new ZX9 active speaker. It's a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
+      link: "/speakers/zx9-speaker",
+    },
+    {
+      img: zx7,
+      name: "ZX7 SPEAKER",
+      desc: "Stream high quality sound wirelessly with minimal loss. The ZX7 bookshelf speaker uses high-end audiophile components that represents the top of the line powered speakers for home or studio use.",
+      link: "/speakers/zx7-speaker",
+    },
+  ];
 
-  return <ProductPage heading={heading} />;
+  return <ProductPage heading={heading} products={products} />;
 };
 
 export default SpeakersPage;
