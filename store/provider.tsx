904d29cdff38cec9f4abbe7e1ef84caa3a92381b
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-06-24 15:27:42 +0100

    persisted state with redux persist

--- store/provider.tsx
+++ store/provider.tsx
@@ -1,8 +1,15 @@
 "use client";
 
-import store from "./store";
+import { store, persistedStore } from "./store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistedStore}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
--- store/store.ts
+++ store/store.ts
@@ -1,13 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
 import audiophileReducer from "./audiophileSlice";
 
-const store = configureStore({
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, audiophileReducer);
+
+export const store = configureStore({
   reducer: {
-    appState: audiophileReducer,
+    appState: persistedReducer,
   },
+  middleware: [thunk],
 });
 
-export default store;
+export const persistedStore = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
